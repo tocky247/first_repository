48b6e79c165a160b7cd4e46825b519beaf2be660
tocky247 <s14591jt@sfc.keio.ac.jp>
2014-10-04 22:25:04 +0900

    第２回課題

---
+++ 第２回課題.js
@@ -0,0 +1,18 @@
+function add(a,b){
+ return a+b;
+}
+
+function multiply(c,d){
+ return c*d;
+}
+
+function apple(x){
+var y = multiply(2,x);
+var z = add(y,10);
+ return z
+}
+
+apple(apple(apple(8)));
+/*
+134
+*/
